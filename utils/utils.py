e1f20659b3c5cf18f4ba36e1de08ff69ef94261e
mdlangeles <maria.amu@uao.edu.co>
2024-05-04 16:04:30 -0500

    commit prueba

---
+++ utils/utils.py
@@ -0,0 +1,22 @@
+import country_converter as coco
+import pandas as pd
+
+
+def continent(country):
+    cc = coco.CountryConverter()
+    try:
+        continent = cc.convert(names=country, to='continent')
+        return continent
+    except:
+        return None
+    
+
+def del_cols(df):
+    df.drop(columns=['country', 'happiness_rank'], inplace=True)
+    return df
+
+
+
+def dummies(df):
+    df = pd.get_dummies(df, columns=['continent'], prefix='continent')
+    return df
